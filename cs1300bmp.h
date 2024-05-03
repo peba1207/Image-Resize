cb643fb269f8d472117d7f439c8ebe44cdc0e3f7
Peyton Barger <peba1207@colorado.edu>
2024-05-03 07:11:03 -0600

    linear interpolation

---
+++ cs1300bmp.h
@@ -0,0 +1,59 @@
+//
+// Created by peyto on 4/23/2024.
+//
+
+#ifndef CTSIM_CS1300BMP_H
+#define CTSIM_CS1300BMP_H
+
+
+//-*-c++-*-
+#ifndef _cs1300bmp_h_
+#define _cs1300bmp_h_
+
+//
+// Maximum image size
+//
+#define MAX_DIM 512
+
+//
+// The specific colors
+//
+#define COLOR_RED 0
+#define COLOR_GREEN 1
+#define COLOR_BLUE 2
+#define MAX_COLORS 3
+
+struct cs1300bmp {
+    //
+    // Actual width used by this image
+    //
+    int width;
+    //
+    // Actual height used by this image
+    //
+    int height;
+    //
+    // R/G/B fields
+    //
+    unsigned char color[MAX_COLORS][MAX_DIM][MAX_DIM];
+};
+
+//
+// routines to read and write BMP images
+//
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int cs1300bmp_readfile(char *filename, struct cs1300bmp *image);
+int cs1300bmp_writefile(char *filename, struct cs1300bmp *image);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
+
+#endif //CTSIM_CS1300BMP_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,69 @@
 #include <iostream>
-
+#include <string>
+#include "cs1300bmp.h"
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //using the cs1300bmp file from another class
+    //turns a bmp file into a 2D array of rgb values
+    struct cs1300bmp *input = new struct cs1300bmp;
+    string filename = "C:\\Users\\peyto\\CLionProjects\\imageResize\\me.bmp";
+    int ok = cs1300bmp_readfile((char *) filename.c_str(),input);
+
+    const int scale = 8;
+    int width = input->width;
+    int height = input->height;
+
+    if(ok){
+        cout << "file loaded";
+
+        struct cs1300bmp *linear = new struct cs1300bmp;
+        linear->width = (input->width*scale)-(scale-1);
+        linear->height = (input->height*scale)-(scale-1);
+
+        for(int h = 0; h<3; h++) {
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    linear->color[h][i*scale][j*scale] = /*255;//*/input->color[h][i][j];
+                }
+            }
+        }
+        //convert the array into an image
+        string outputName = "linear0.bmp";
+        cs1300bmp_writefile((char *) outputName.c_str(), linear);
+
+        for(int h = 0; h<3; h++) {
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height - 1; j++) {
+                    int v1 = input->color[h][j][i];
+                    int v2 = input->color[h][(j + 1)][i];
+                    double delta = (double) (v2 - v1) / scale;
+                    for (int k = 1; k < scale; k++) {
+                        linear->color[h][j * scale + k][i * scale] = (unsigned char) (k * delta) + v1;
+                    }
+                }
+            }
+        }
+        //convert the array into an image
+        outputName = "linear1.bmp";
+        cs1300bmp_writefile((char *) outputName.c_str(), linear);
+        //interpolate horizonally
+        for(int h = 0; h<3; h++) {
+            for (int i = 0; i < linear->height; i++) {
+                for (int j = 0; j < width-1; j++) {
+                    int v1 = linear->color[h][i][j*scale];
+                    int v2 = linear->color[h][i][(j+1)*scale];
+                    double delta = (double) (v2 - v1) / scale;
+                    for (int k = 1; k <= scale; k++) {
+                        linear->color[h][i][j * scale + k] = (unsigned char) (k * delta) + v1;
+                    }
+                }
+            }
+        }
+        outputName = "linear2.bmp";
+        cs1300bmp_writefile((char *) outputName.c_str(), linear);
+    }
+
+
     return 0;
+
 }
